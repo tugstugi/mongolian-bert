2727ee5b13466c3bdd83209baa2749c55b77a938
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-03-24 04:33:51 +0800

    html content parser

--- bookextractor/epub.py
+++ bookextractor/epub.py
@@ -2,10 +2,23 @@ import sys
 import os
 import ebooklib
 from ebooklib import epub
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        pass
+    def handle_endtag(self, tag):
+        pass
+    def handle_data(self, data):
+        print(data)
+
+parser = MyHTMLParser()
 
 if (len(sys.argv)>1):
     bookpath = sys.argv[1]
     book = epub.read_epub(bookpath)
-    for image in book.get_items_of_type(ebooklib.ITEM_IMAGE):
-        print(image)
+    for doc in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        content = doc.get_content().decode("utf-8") 
+        content = parser.feed(content)
+        print(content)
     pass
