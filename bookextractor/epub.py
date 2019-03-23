f973051d959ff59f0cbeb3de83691d744ee1349d
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-03-24 04:23:07 +0800

    reading epub

--- bookextractor/epub.py
+++ bookextractor/epub.py
@@ -1,7 +1,11 @@
 import sys
 import os
+import ebooklib
+from ebooklib import epub
 
 if (len(sys.argv)>1):
-    print("epub")
-    print(sys.argv[1])
+    bookpath = sys.argv[1]
+    book = epub.read_epub(bookpath)
+    for image in book.get_items_of_type(ebooklib.ITEM_IMAGE):
+        print(image)
     pass
--- preprop_mn_books.sh
+++ preprop_mn_books.sh
@@ -5,8 +5,8 @@ mkdir -p ebooks/
 for file in ebooks/*
 do
   if [ "${file##*.}" == "epub" ]; then
-    python bookextractor/epub.py $(pwd)"$file"
+    python bookextractor/epub.py $(pwd)/"$file"
   elif [ "${file##*.}" == "mobi" ]; then
-    python bookextractor/mobi.py $(pwd)"$file"
+    python bookextractor/mobi.py $(pwd)/"$file"
   fi
 done
