9c2bca8ddcbe611373b0875e17f77ea0e3a2d1d5
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-03-24 05:01:31 +0800

    epub to bert format

--- bookextractor/epub.py
+++ bookextractor/epub.py
@@ -1,8 +1,18 @@
 import sys
 import os
+import re
 import ebooklib
 from ebooklib import epub
 from html.parser import HTMLParser
+import nltk
+from nltk import tokenize
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+texts = ""
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -10,7 +20,13 @@ class MyHTMLParser(HTMLParser):
     def handle_endtag(self, tag):
         pass
     def handle_data(self, data):
-        print(data)
+        global texts
+        texts = texts+"\n"
+        sentences = tokenize.sent_tokenize(data)
+        if len(sentences)!=0:
+            for sentence in sentences:
+                texts = texts + sentence+"\n"
+                #print(sentence)
 
 parser = MyHTMLParser()
 
@@ -19,6 +35,8 @@ if (len(sys.argv)>1):
     book = epub.read_epub(bookpath)
     for doc in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
         content = doc.get_content().decode("utf-8") 
-        content = parser.feed(content)
-        print(content)
+        parser.feed(content)
+    # reduce multiple empty lines to one
+    texts = re.sub(r'\n\s*\n', '\n\n', texts)
+    print(texts)
     pass
